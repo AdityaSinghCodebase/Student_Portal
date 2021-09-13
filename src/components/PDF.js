5bfbad9521288cb6d00cea15cd5e186a9d2f0991
Adityasinghtomar3o3 <67886796+Adityasinghtomar3o3@users.noreply.github.com>
2021-09-13 06:41:34 +0530

    final set

--- src/components/PDF.js
+++ src/components/PDF.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Pdf from "react-to-pdf";
 
 const ref = React.createRef();
@@ -7,15 +7,52 @@ const PDF = (props) => {
   return (
     <>
       <div className="Post" ref={ref}>
-        <h1>{props.title}</h1>
-        <img src={props.image} alt={props.title} />
-        <p>{props.content}</p>
+        <div>
+          <h1>Student Admit Card 2021</h1>
+          <br />
+
+          <table class="table">
+            <tr>
+              <th scope="row">Name</th>
+              <td>{props.name}</td>
+            </tr>
+            <tr>
+              <th scope="row">Father Name</th>
+              <td>{props.fatherName}</td>
+            </tr>
+            <tr>
+              <th scope="row">Mobile Number</th>
+              <td>{props.fatherMobile}</td>
+            </tr>
+            <tr>
+              <th scope="row">Class</th>
+              <td>{props.class}</td>
+            </tr>
+            <tr>
+              <th scope="row">School</th>
+              <td>{props.schoolName}</td>
+            </tr>
+            <tr>
+              <th scope="row">Address</th>
+              <td>{props.address}</td>
+            </tr>
+          </table>
+
+          <br />
+          <br />
+
+          <p style={{ textAlign: "right" }}>Auth. Signature</p>
+        </div>
       </div>
-      <Pdf targetRef={ref} filename="post.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>Capture as PDF</button>}
+      <Pdf targetRef={ref} filename="admitCard.pdf">
+        {({ toPdf }) => (
+          <button className="btn-outline-success btn-lg" onClick={toPdf}>
+            Print your Admit Card
+          </button>
+        )}
       </Pdf>
     </>
   );
-}
+};
 
 export default PDF;
